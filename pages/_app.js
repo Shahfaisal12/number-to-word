16a1806d163b5059a347073d66c2859c06eec7c5
shahfaisal12 <faisalmkd02@gmail.com>
2022-12-05 14:25:53 +0500

    'add'

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
+
   <>
-  <Component {...pageProps} />
+  <Component {...pageProps} />;
   </>
-  )
+
 }
 
 export default MyApp;
