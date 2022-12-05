da2539addd53d3f0e82e3575b9386aaa510faf47
shahfaisal12 <faisalmkd02@gmail.com>
2022-12-05 14:52:53 +0500

    'addjs'

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
 
-  <>
-  <Component {...pageProps} />;
-  </>
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return<Component {...pageProps} />;
 
 }
 
 export default MyApp;
+
+
+
